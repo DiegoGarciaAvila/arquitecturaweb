bb1297e651e0a2adde4fc736f163262e494e79b4
DiegoGarciaAvi <diego_201723075@test.edu.mx>
2023-09-22 17:33:51 -0600

    Se agregaron estilos a proyects, falta estilos a lista

--- js/index.js
+++ js/index.js
@@ -2,3 +2,17 @@ $(document).ready(function() {
 // Inicializa el carrusel
     $('#miCarrusel').carousel();
 });
+
+fetch('../components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    });
+   
+
+fetch('../components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('fooder-container').innerHTML = data;
+    });
+
--- js/proyects.js
+++ js/proyects.js
@@ -4,3 +4,41 @@ function ajustarAlturaIframe() {
 }
 window.addEventListener('load', ajustarAlturaIframe);
 window.addEventListener('resize', ajustarAlturaIframe);
+
+fetch('../components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    });
+
+fetch('../components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('fooder-container').innerHTML = data;
+    });
+
+fetch('../proyects/proyect1.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('proyecto1').innerHTML = data;
+    });
+
+fetch('../proyects/proyect2.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('proyecto2').innerHTML = data;
+    });
+
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        event.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing');
+    });
+});
